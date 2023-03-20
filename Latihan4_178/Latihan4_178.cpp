3d8d2fcfba80e1f341e9575860f6354e27a1255a
sudirboyy <sudirboyy@gmail.com>
2023-03-20 15:16:21 +0700

    Membuat input untuk user dan menambahkan while

--- Latihan4_178/Latihan4_178.cpp
+++ Latihan4_178/Latihan4_178.cpp
@@ -8,7 +8,7 @@ float LuasLingkaran(float r) {
 	return 3.14 * r * r;
 }
 float LuasSegitiga(float a, float t) {
-	return 0.5 * a * t
+	return 0.5 * a * t;
 }
 
 int main()
@@ -24,5 +24,35 @@ int main()
 		cout << " 2. Luas Lingkaran " << endl;
 		cout << " 3. Luas Segitiga " << endl;
 		cout << " 4. Exit " << endl;
+		cout << "PILIHAN (1/2/3/4) : " << endl;
 		cin >> pilihan;
 
+		switch (pilihan)
+		{
+		case 1:
+			cout << "Masukkan Panjang = ";
+			cin >> panjang;
+			cout << "Masukkan Lebar = ";
+			cin >> lebar;
+			cout << "Luas Persegi Panjang = " << LuasPersegi(panjang, lebar);
+			break;
+		case 2:
+			cout << "Masukkan jari - jari = ";
+			cin >> jejari;
+			cout << "Luas Lingkaran = " << LuasLingkaran(jejari);
+			break;
+		case 3:
+			cout << "Masukkan Alas = ";
+			cin >> alas;
+			cout << "Masukkan Tinggi = ";
+			cin >> tinggi;
+			cout << "Luas Segitiga = " << LuasSegitiga(alas, tinggi);
+			break;
+		case 4:
+			break;
+		default:
+			cout << "PILIHAN SALAH" << endl;
+			break;
+		}
+	} while (pilihan != 4);
+}
